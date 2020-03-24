3147f27259fe340e4bdc9dd12fc5368a1cfd2dfe
Quinn Webber <webber.quinn@gmail.com>
2020-03-24 13:56:21 -0230

    Update 2-NN.R

---
+++ scripts/2-NN.R
@@ -0,0 +1,56 @@
+
+## Cleaned Locs - Calculate NN ====
+
+### Packages ----
+libs <- c( 'ggplot2', 'rgdal',  'data.table',
+          'spatsoc', 'igraph', 'asnipe')
+lapply(libs, require, character.only = TRUE)
+
+#devtools::install_github("rOpenSci/spatsoc")
+
+## set variables
+projCols <- c('EASTING', 'NORTHING')
+
+
+### Input raw data ----
+DT <- readRDS('output/1-clean-all.Rds')
+
+# Temporal grouping 
+DT <- group_times(DT, datetime = 'datetime', threshold = '5 minutes')
+
+## new column so we can merge NN values back to main dataset
+DT$IDYrTime <- as.factor(paste(DT$IDYr, DT$timegroup, sep = "_"))
+
+
+#####################################################
+############# CALCULDATE DISTANCE ##################
+####################################################
+
+## Nearest neighbor at end step
+edist <- edge_nn(DT = DT, id = 'IDYr', coords = c('EASTING', 'NORTHING'),
+                   timegroup = 'timegroup', threshold = 45000, returnDist = TRUE, 
+                   #fillNA = FALSE,
+                   splitBy = c("Year"))
+
+## add the same column as above to merge NN values with main dataset
+edist$IDYrTime <- as.factor(paste(edist$ID, edist$timegroup, sep = "_"))
+
+## remove columns already in main dataset
+edist[, c("Year", "timegroup", "ID") := NULL]
+
+DT <- merge(DT, edist, by = "IDYrTime")
+
+## Generate spatial groups
+DT <- group_pts(
+  DT,
+  threshold = 50,
+  splitBy = c('Year'),
+  timegroup = 'timegroup',
+  id = 'ANIMAL_ID',
+  coords = projCols
+)
+
+## Export data
+saveRDS(DT, 'output/2-clean-all-nn.Rds')
+
+
