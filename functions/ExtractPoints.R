aa9e3333dea5178ed2ecdb9a6ffd0a1cca86d619
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-25 16:53:53 -0300

    rm extract clearer if we just use the extract function directly

--- functions/ExtractPoints.R
+++
@@ -1,6 +0,0 @@
-
-
-ExtractPoints <- function(pt.matrix, raster.layer){
-  # Extract in raster layer values at points
-  raster::extract(raster.layer, pt.matrix) ## extract function needs to be from 'raster' package
-}
