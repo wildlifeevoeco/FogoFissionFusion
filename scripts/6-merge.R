3d33456e8c661188aa6dee0bc627531089f3fc05
Quinn Webber <webber.quinn@gmail.com>
2020-05-06 14:26:35 -0230

    Update 6-merge.R

--- scripts/6-merge.R
+++ scripts/6-merge.R
@@ -4,4 +4,8 @@ library(data.table)
 
 sri <- readRDS('output/4-sri.RDS')
 hr <- readRDS('output/5-hro.Rds')
-body <-
+body <- fread('input/body.csv')
+
+body2 <- body[,c("date") := NULL][, lapply(.SD, mean), by=ANIMAL_ID]
+body2
+
