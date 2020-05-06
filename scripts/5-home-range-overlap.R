4f43c24efc3c051323108000bd024510029a0f6b
Quinn Webber <webber.quinn@gmail.com>
2020-05-06 14:10:14 -0230

    Update 5-home-range-overlap.R

--- scripts/5-home-range-overlap.R
+++ scripts/5-home-range-overlap.R
@@ -6,7 +6,7 @@ libs <- c('data.table', 'sp', 'adehabitatHR')
 lapply(libs, require, character.only = TRUE)
 
 ### Input data ----
-locs <- readRDS('derived-data/cleaned-locs.Rds')
+locs <- readRDS('output/1-clean-all.Rds')
 
 ### Calculate Home range area for each individual ----
 utm21N <- '+proj=utm +zone=21 ellps=WGS84'
@@ -14,14 +14,14 @@ utm21N <- '+proj=utm +zone=21 ellps=WGS84'
 coords <- c('EASTING', 'NORTHING')
 pts <- SpatialPointsDataFrame(locs[, ..coords],
                               proj4string = CRS(utm21N),
-                              data = locs[, .(ID)])
+                              data = locs[, .(ANIMAL_ID)])
 
 ud <- kernelUD(pts, grid = 700, extent = 3)
 vertices <- getverticeshr(ud, 95)
 vert.dt <- as.data.table(vertices)
 
 # Split up paste ID
-vert.dt[, c('ANIMAL_ID', 'season', 'Year', 'HERD') := tstrsplit(id, '_')]
+vert.dt[, c('ANIMAL_ID', 'Year') := tstrsplit(id, '_')]
 
 # convert from ha to km2
 vert.dt[, areaKM2 := area/100]
