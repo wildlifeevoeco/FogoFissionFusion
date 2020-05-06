59e4ff88edce93ab16375ef92838d5a4ca79432e
Quinn Webber <webber.quinn@gmail.com>
2020-05-06 14:09:09 -0230

    get_sri function

--- functions/get_sri.R
+++ functions/get_sri.R
@@ -1,10 +1,6 @@
-#' Dynamic network
+#' get_sri
 #'
-#' @inheritParams hr_network
 #'
-#' @return Graph strength for each individual. 
-#' @export
-#' 
 get_sri <- function(DT = NULL, id = NULL, by = NULL) {
   
   if (is.null(DT) | is.null(id)) {
@@ -12,12 +8,12 @@ get_sri <- function(DT = NULL, id = NULL, by = NULL) {
   }
   
   DT[, {
-    d <- data.table::dcast(.SD, formula = groupEnd ~ get(id), 
-                           fun.aggregate = length, value.var = 'groupEnd')
+    d <- data.table::dcast(.SD, formula = group ~ get(id), 
+                           fun.aggregate = length, value.var = 'group')
     
-    gbi_df <- data.matrix(d[, !'groupEnd', with = FALSE])
+    gbi_df <- data.matrix(d[, !'group', with = FALSE])
     
-    rownames(gbi_df) <- d$groupEnd
+    rownames(gbi_df) <- d$group
     
     gbi.net_df <- asnipe::get_network(gbi_df, data_format = "GBI", 
                                       association_index = "SRI")
@@ -34,15 +30,6 @@ get_sri <- function(DT = NULL, id = NULL, by = NULL) {
     out <- data.table(ID1 = out$Var1, 
                       ID2 = out$Var2, 
                       sri = out$value)
-    
-    
-    #memb <- data.table(membershipID1 = membership(fastgreedy.community(gbi.grph_df)),
-    #                   ID1 = names(igraph::degree(gbi.grph_df)))
-    #memb2 <- data.table(membershipID2 = membership(fastgreedy.community(gbi.grph_df)),
-    #                   ID2 = names(igraph::degree(gbi.grph_df)))
-    
-    #all <- merge(out, memb, by = "ID1")                   
-    #all2 <- merge(all, memb2, by = "ID2")                  
     
   }, by = by]
 }
