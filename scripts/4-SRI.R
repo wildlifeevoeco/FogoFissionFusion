0b11dd27e46123d975a0cc71075708d0d33be452
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-07 19:30:52 +0000

    add alloc.col since reading RDS files

--- scripts/4-SRI.R
+++ scripts/4-SRI.R
@@ -1,18 +1,21 @@
-
-
+### SRI ====
 
 
 ### Packages ----
 libs <- c('data.table', 'spatsoc')
 lapply(libs, require, character.only = TRUE)
 
+### Functions ----
+source('functions/get_sri.R')
+
+
 ### Input data ----
 locs <- readRDS('output/1-clean-all.Rds')
+alloc.col(locs)
 
 ### Proximity Based Social Networks ----
-
 # Temporal grouping 
-locs <- group_times(locs, datetime = 'datetime', threshold = '5 minutes')
+group_times(locs, datetime = 'datetime', threshold = '5 minutes')
 
 group_pts(
   locs,
@@ -23,12 +26,10 @@ group_pts(
   coords = c('EASTING', 'NORTHING')
 )
 
-### Calculate SRI for each year ----
-source("functions/get_sri.R")
+# Calculate SRI for each year
+nets <- get_sri(locs, id = 'ANIMAL_ID', by = 'Year')
 
-nets <- get_sri(locs, id = 'ANIMAL_ID', by = c('Year'))
+nets[, dyad := as.factor(paste(ID1, ID2, sep = '_'))]
 
-nets$dyad <- as.factor(paste(nets$ID1, nets$ID2, sep = "_"))
-
-saveRDS(nets, "output/4-sri.RDS")
-
+### Output ----
+saveRDS(nets, 'output/4-sri.RDS')
