557d0ccb19b120dd0f07490b763978f23309a808
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-07 17:01:38 -0300

    tidy

--- scripts/3-Habitat.R
+++ scripts/3-Habitat.R
@@ -8,12 +8,9 @@ lapply(libs, require, character.only = TRUE)
 ### Input raw data ----
 DT <- readRDS('output/2-clean-all-nn.Rds')
 
-lcFogo <- raster("../nl-landcover/output/fogo_lc.tif")
-legend <- fread("../nl-landcover/input/FINAL_PRODUCT/FINAL_RC_legend.csv")
+lcFogo <- raster('../nl-landcover/output/fogo_lc.tif')
+legend <- fread('../nl-landcover/input/FINAL_PRODUCT/FINAL_RC_legend.csv')
 
-### Variables ----
-utm21N <- '+proj=utm +zone=21 ellps=WGS84'
-crs <- CRS("+proj=utm +zone=14 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 
 ### Plot landcover ----
 spplot(lcFogo)
@@ -44,13 +41,13 @@ names(focals) <- legend$Value
 
 # combine habitat types into groupings of your choice 
 # using the Value numbers from the legend
-openMove <- Reduce("+", focals[c(1, 6, 9)])
-forest <- Reduce("+", focals[c(2, 3, 4, 5)])
+openMove <- Reduce('+', focals[c(1, 6, 9)])
+forest <- Reduce('+', focals[c(2, 3, 4, 5)])
 lichen <- focals[[8]]
 
-### This step makes new raster layers that are "proportion of habitat within a 100 m 
-# buffer that is habitat x". Tends to make analyses more robust and less susceptible
-# to problems with autocorrelation.-MPL
+### This step makes new raster layers that are 'proportion of habitat within a 100 m 
+# buffer that is habitat x'. Tends to make analyses more robust and less susceptible
+# to problems with autocorrelation-MPL
 
 # Generate buffer size
 # you can change this number, but right now it is a 100m circle around each point
@@ -67,6 +64,7 @@ DT[, propOpenMove := extract(openMoveBuff100, matrix(c(EASTING, NORTHING), ncol 
 DT[, propForest := extract(ForestBuff100, matrix(c(EASTING, NORTHING), ncol = 2))]
 DT[, propLichen := extract(LichenBuff100, matrix(c(EASTING, NORTHING), ncol = 2))]
 
-str(DT)
 
-saveRDS(DT, "output/3-clean-all-nn-hab.RDS")
+
+### Output ----
+saveRDS(DT, 'output/3-clean-all-nn-hab.RDS')
