df221797e81c56c81affbad760cf4679f16f20b5
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-07 23:22:29 -0230

    add diff_dyad function

--- scripts/6-Body-Size.R
+++ scripts/6-Body-Size.R
@@ -9,24 +9,45 @@ lapply(libs, require, character.only = TRUE)
 ### Input ----
 body <- fread('input/body.csv')
 
+idcol <- 'ANIMAL_ID'
+
+### Functions ----
+#' @param DT input data.table 
+#' @param col column to measure difference in
+#' @param id id column
+#' 
+#' @example
+#' diff_dyad(bodyavg, 'total_length', 'ANIMAL_ID')
+diff_dyad <- function(DT, col, id) {
+  d <- as.matrix(dist(DT[[col]]))
+  d[lower.tri(d, diag = TRUE)] <- NA
+  dimnames(d) <- list(DT[[id]], DT[[id]])
+  
+  lendiff <- data.table(
+    ID1 = rep(colnames(d), each = nrow(d)),
+    ID2 = rep(rownames(d), ncol(d)),
+    diff = as.vector(d)
+  )[!is.na(diff)]
+  setnames(lendiff, 'diff', paste0('diff_', col))
+  dyad_id(lendiff, 'ID1', 'ID2')
+}
+
 
 ### Body size ----
 bodyavg <- body[, lapply(.SD, mean, na.rm = T), 
-                by = ANIMAL_ID, .SDcols = -"date"]
+                by = idcol, .SDcols = -'date']
 
 
-# Calculate difference in size between all individuals
-d <- as.matrix(dist(body2$total_length))
-d[lower.tri(d, diag = TRUE)] <- NA
-dimnames(d) <- list(body2$ANIMAL_ID, body2$ANIMAL_ID)
+# Calculate difference between all individuals
+varls <- bodyavg[, names(.SD), .SDcols = -idcol]
 
-lendiff <- data.table(
-  ID1 = rep(colnames(d), each = nrow(d)),
-  ID2 = rep(rownames(d), ncol(d)),
-  diff = as.vector(d)
-)[!is.na(diff)]
+# eg. for one variable
+# diff_dyad(DT = bodyavg, col = 'total_length', id = idcol)
 
-dyad_id(lendiff, 'ID1', 'ID2')
+Reduce(function(x, y) merge(x, y, on = 'dyadID'), lapply(varls, diff_dyad, DT = bodyavg, id = idcol))
+
+
+
 
 # Check
 lendiff[sample(1), diff] == body2[ANIMAL_ID %in% c(lendiff[1, ID1], lendiff[1, ID2]), dist(total_length)]
