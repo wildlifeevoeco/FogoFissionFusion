490cdba9623edd2e06f3026e616c47cb6ec058a1
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-10 00:28:01 -0230

    drop idyrtime, just use merge directly

--- scripts/2-NN.R
+++ scripts/2-NN.R
@@ -1,4 +1,4 @@
-### Cleaned Locs - Calculate NN ====
+### Calculate NN ====
 
 ### Packages ----
 libs <- c( 'ggplot2', 'rgdal',  'data.table',
@@ -17,33 +17,25 @@ projCols <- c('EASTING', 'NORTHING')
 ### Temporal grouping ----
 group_times(DT, datetime = 'datetime', threshold = '5 minutes')
 
-# new column so we can merge NN values back to main dataset
-DT[, IDYrTime := as.factor(paste(IDYr, timegroup, sep = '_'))]
-
-
 
 ### Nearest neighbor at end step ----
 edges <-
   edge_nn(
     DT = DT,
-    id = 'IDYr',
+    id = 'ANIMAL_ID',
     coords = c('EASTING', 'NORTHING'),
     timegroup = 'timegroup',
-    threshold = 45000,
+    returnDist = TRUE,
     splitBy = c('Year')
   )
 
-# add the same column as above to merge NN values with main dataset
-edges[, IDYrTime := as.factor(paste(ID, timegroup, sep = '_'))]
-
-# remove columns already in main dataset
-edges[, c('Year', 'timegroup', 'ID') := NULL]
-
-DT <- merge(DT, edges, by = 'IDYrTime')
+out <- merge(DT, edges, 
+            by.x = c('ANIMAL_ID', 'timegroup', 'Year'),
+            by.y = c('ID', 'timegroup', 'Year'))
 
 ### Generate spatial groups ----
 group_pts(
-  DT,
+  out,
   threshold = 50,
   splitBy = c('Year'),
   timegroup = 'timegroup',
@@ -51,7 +43,8 @@ group_pts(
   coords = projCols
 )
 
+
 ### Export data ----
-saveRDS(DT, 'output/2-clean-all-nn.Rds')
+saveRDS(out, 'output/2-clean-all-nn.Rds')
 
 
