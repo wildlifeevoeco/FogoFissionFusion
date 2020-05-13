739243fdf7f369993dda99a416080e83c7dc7c0d
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-13 20:21:38 -0230

    nice sections, tidy

--- scripts/04-SRI.R
+++ scripts/04-SRI.R
@@ -1,37 +1,42 @@
-### SRI ====
+# === SRI ---------------------------------------------------------------------
 
 
-### Packages ----
+# Packages ----------------------------------------------------------------
 libs <- c('data.table', 'spatsoc')
 lapply(libs, require, character.only = TRUE)
 
 
-### Input data ----
-DT <- readRDS('output/1-clean-all.Rds')
+# Input data --------------------------------------------------------------
+DT <- readRDS('output/1-prep-locs.Rds')
 alloc.col(DT)
 
 
-### Functions ----
+# Functions ---------------------------------------------------------------
 source('functions/get_sri.R')
 
 
-### Proximity Based Social Networks ----
-# Temporal grouping 
+
+# Proximity based social networks -----------------------------------------
+## Temporal grouping 
 group_times(DT, datetime = 'datetime', threshold = '5 minutes')
 
+## Spatial grouping
 group_pts(
   DT,
   threshold = 50,
-  splitBy = c('Year'),
+  splitBy = 'Year',
   timegroup = 'timegroup',
   id = 'ANIMAL_ID',
   coords = c('EASTING', 'NORTHING')
 )
 
-# Calculate SRI for each year
+
+# Calculate SRI for each year ---------------------------------------------
 nets <- get_sri(DT, id = 'ANIMAL_ID', by = 'Year')[!is.na(sri)]
 
+# Set dyad id
 dyad_id(nets, 'ID1', 'ID2')
 
-### Output ----
-saveRDS(nets, 'output/4-sri.Rds')
+
+# Output ------------------------------------------------------------------
+saveRDS(nets, 'output/3-sri.Rds')
