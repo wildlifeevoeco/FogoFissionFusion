73f020e727c8c4419a240c7954f584cf8d24f806
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-13 21:44:43 -0230

    new inputs

--- scripts/08-Merge.R
+++ scripts/08-Merge.R
@@ -7,10 +7,13 @@ lapply(libs, require, character.only = TRUE)
 
 
 # Input -------------------------------------------------------------------
-sri <- readRDS('output/4-sri.Rds')
-hro <- readRDS('output/5-hro.Rds')
-body <- readRDS('output/6-body-size-diffs.Rds')
+sri <- readRDS('output/03-sri.Rds')
+hro <- readRDS('output/05-hro.Rds')
+body <- readRDS('output/06-body-size-diffs.Rds')
 
+dyads <- readRDS('output/07-dyads.Rds')
+
+# TODO: merge dyads? use where
 
 # Merge -------------------------------------------------------------------
 # List data.tables
