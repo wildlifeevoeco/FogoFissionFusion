b95f83fcb73de9b20b43bb3a35a070834cdd984e
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-20 19:16:20 -0230

    calc last loc after dropping for season NAs

--- scripts/01-Prep-Locs.R
+++ scripts/01-Prep-Locs.R
@@ -33,18 +33,19 @@ fogo[, itime := as.ITime(itime, tz = tz)]
 fogo[, datetime := as.POSIXct(paste(idate,itime), 
                               format = '%Y-%m-%d %H:%M:%S', tz = tz)]
 
-# Add relocation ID by individual and determine last loc for each individual
-setorder(fogo, 'datetime')
-
-fogo[, locID := rleid(datetime), by = ANIMAL_ID]
-fogo[, lastLoc := locID == max(locID), by = ANIMAL_ID]
-
 # ID by Yr
 fogo[, IDYr := paste(ANIMAL_ID, Year, sep = '_')]
 
 # Winter season (Jan 1 to March 16)
 fogo[JDate >= 1 & JDate <= 75, season := 'winter']
 fogo <- fogo[!is.na(season)]
+
+# Add relocation ID by individual and determine last loc for each individual
+setorder(fogo, 'datetime')
+
+fogo[, locID := rleid(datetime), by = ANIMAL_ID]
+fogo[, lastLoc := locID == max(locID), by = ANIMAL_ID]
+
 
 # UTM zone 21N
 fogo[, (projCols) := as.data.table(project(cbind(X_COORD, Y_COORD), utm21N))]
