bc45e57943ca314f59b7c42bf1379a082347071a
Quinn Webber <webber.quinn@gmail.com>
2020-06-19 11:02:41 -0230

    add figure for number of dyad pts by habitat

--- scripts/Descriptive plots fusion.R
+++ scripts/Descriptive plots fusion.R
@@ -9,6 +9,63 @@ fusion=readRDS('output/07-dyads.Rds')
 
 Fusion=subset(fusion, fusion0==TRUE)
 
+fusion[, count_fuse := length(fusion0), by = .(fusion0)]
+
+
+## color palette
+cbPalette <- c("#999999", "red", "#56B4E9", "#009E73", 
+               "#F0E442", "#0072B2", "#D55E00", "#CC79A7","#E69F00")
+
+## function to re-order bars on figure
+reorder_size <- function(x) {
+  factor(x, levels = names(sort(table(x), decreasing = T)))
+}
+
+## delete NAs from dyad habitat type
+fusion <- fusion[!is.na(dyadLC)]
+
+## change name of false/true to fission/fusion
+fusion$fusion0[fusion$fusion0 == FALSE] <- "fused"
+fusion$fusion0[fusion$fusion0 == TRUE] <- "fusion"
+
+## change name of anthro
+fusion$dyadLC[fusion$dyadLC == "Anthropogenic and disturbance"] <- "Anthropogenic"
+
+
+a1 <- ggplot(fusion[fusion0 == "fusion"], aes(reorder_size(dyadLC), count_fuse, fill=dyadLC)) +
+  stat_summary(fun = "sum", geom="bar") +
+  xlab('') +
+  ylab('Frequency') +
+  ggtitle('a) dyad fusion') +
+  ylim(0,5000) +
+  scale_fill_manual(values=cbPalette) + 
+  theme(legend.position = 'none',
+        axis.text.x=element_text(size=12, color = "black", angle = 45, hjust = 1),
+        axis.text.y=element_text(size=14, color = "black"),
+        axis.title.y=element_text(size=14),
+        strip.text = element_text(size=14,face = "bold"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))
+b1 <- ggplot(fusion[fusion0 == "fused"], aes(reorder_size(dyadLC), count_fuse, fill=dyadLC)) +
+  stat_summary(fun = "sum", geom="bar") +
+  xlab('') +
+  ylab('Frequency') +
+  ggtitle('b) dyads fused for >1 time step') +
+  ylim(0,5000) +
+  scale_fill_manual(values=cbPalette) + 
+  theme(legend.position = 'none',
+        axis.text.x=element_text(size=12, color = "black", angle = 45, hjust = 1),
+        axis.text.y=element_text(size=14, color = "black"),
+        axis.title.y=element_text(size=14),
+        strip.text = element_text(size=14,face = "bold"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1))
+grid.arrange(a1, b1, nrow = 1)
+
+
+
 #mosaic graph
 
 #extract what I need: landcover at a dyad centroid and dyadLC
