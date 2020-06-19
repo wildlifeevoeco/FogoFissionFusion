99487f8af03c4b3d9c4b2fc033e03a178acf9d7f
Morgane le Goff <morganelegoff@MacBook-Air-de-Morgane-2.local>
2020-06-19 17:40:43 -0230

    kaplan-meier work

--- scripts/B- Hypothesis 2.1 - surv. analysis.R
+++ scripts/B- Hypothesis 2.1 - surv. analysis.R
@@ -60,3 +60,67 @@ fit1
 ##fit8=update(fit7,.~.-ShanIndex)
 fit8
 
+
+############### KM part #######
+
+
+#input data
+survi=readRDS("output/07-dyads.Rds")
+
+head(survi)
+
+#have to have 1 runcount for each runid
+
+#need data when min=2 --> runCount =/= NA
+survi=subset(survi, min2==TRUE)
+survi[ ,.N,by= runCount]
+survi=survi[,.(Year,dyadID,runCount,DyadDominantLC,censored,runid,mean_open)]
+
+duplicated(survi)  
+dim(survi)
+survi=unique(survi)
+dim(survi)
+
+#censored=1 --> fission event happened, =0, last location is confounded with end of study period
+survi[,.N,by=censored]
+
+#check
+str(survi)
+
+# change class
+survi$DyadDominantLC=as.factor(survi$DyadDominantLC)
+survi$runCount=as.numeric(survi$runCount)
+
+survi[ , runCount:=runCount*2]
+
+##### FIRST ATTEMPT:fit survival analysis using the Kaplan-Meier method
+##### UNIVARIATE:ONLY LANDCOVER (only categorical variable)
+
+surv_object <- Surv(time = survi$runCount, event = survi$censored)
+surv_object  # crosses= censored ind
+
+fit <- survfit(surv_object ~DyadDominantLC , data = survi)
+fit 
+
+ggsurvplot(fit, data = survi,conf.int=TRUE, 
+           pval = TRUE,legend.title = 'Habitat type',
+           legend.labs=c('closed','open'),#surv.median.line = 'hv',
+           ggtheme = theme_bw(),
+           pval.coord = c(110, 0.8),pval.size=4)
+
+# ***: stay longer together when in open habitat
+#crosing curves because gret difference in variance between two groups
+open=subset(survi,DyadDominantLC=='open')
+var(open$runCount)
+closed=subset(survi,DyadDominantLC=='closed')
+var(closed$runCount)
+
+
+#TEST SIGNIFICANCE
+survdiff(surv_object ~ DyadDominantLC, data=survi)
+
+ggplot(survi,aes(x = Year,fill = DyadDominantLC)) + 
+  geom_bar(position = "fill") + labs(fill='Landcover type')+ ylab('Proportion')
+
+plot(survi$mean_open~survi$runCount)
+range(survi$mean_open, na.rm=TRUE)
