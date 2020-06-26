1b6bd815111352ecaac7a82f897fbb7a7ac1ac84
Morgane le Goff <morganelegoff@MacBook-Air-de-Morgane-2.local>
2020-06-26 12:06:58 -0230

    delete the useless

--- scripts/08-Survival.R
+++ scripts/08-Survival.R
@@ -34,19 +34,7 @@ intervals <- dyadsNN[, .(
 # Check:
 # intervals[stop - start != 1]
 
-setorderv(intervals,c('dyadID','stop'),1)
-# seems to work but what does it do to the other columns about the order
-# + some intervals are not one, quid of the shift function right after then?
 
-intervals[,futureEvent:=shift(stayedTogether,n=1, type='lead'),by=.(dyadID,dyadrun)] #why NA???
-intervals[,pastEvent:=shift(stayedTogether,n=1, type='lag'),by=.(dyadID,dyadrun)]
-intervals[,.N,by=futureEvent]
-intervals[,.N,by=pastEvent]
-intervals[,.N,by=stayedTogether]
-
-intervals[, FalseFission := ifelse(stayedTogether == FALSE & futureEvent == TRUE & pastEvent == TRUE,TRUE,FALSE)]
-intervals[,.N,by=FalseFission]
-# no true means no flse fission?
 
 # Output ------------------------------------------------------------------
 saveRDS(intervals, 'output/07-intervals.Rds')
