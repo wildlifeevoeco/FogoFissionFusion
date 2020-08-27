a27fde738e5eeb48ea92cd8fb8dd73fe24a619a4
Morgane le Goff <morganelegoff@macbook-air-de-morgane-2.home>
2020-08-27 15:35:14 +0200

    update

--- scripts/A - GLMM.R
+++ scripts/A - GLMM.R
@@ -4,6 +4,8 @@
 library(lme4)
 library(ggplot2)
 library(visreg)
+library(sjstats)
+library(data.table)
 
 #input data
 data=readRDS("output/6-all-dyad-data.Rds")
@@ -16,11 +18,12 @@ str(data)
 
 # We work with caribou in the same HR (udoi>0)
 HRpos=subset(data,udoi>0)  
+HRpos[ , Difference:= diff_total_length]
 
 # Data exploration
 hist(HRpos$sri, breaks = 20)
 
-plot2=ggplot(HRpos,aes(x=diff_sum_heart_length,y=sri, color=udoi))+geom_point()
+plot2=ggplot(HRpos,aes(x=diff_total_length,y=sri, color=udoi))+geom_point()
 plot2+scale_color_gradient(low="gold", high="dark red")
 
 
@@ -28,9 +31,9 @@ plot2+scale_color_gradient(low="gold", high="dark red")
 
 
 # with diff in (heart girth + total length)
-mod1=lmer(sri~diff_sum_heart_length *udoi +(1|dyadID), data=HRpos)
-mod1.1=lmer(log(sri+1)~diff_sum_heart_length*udoi +(1|dyadID), data=HRpos)
-mod1.2=lmer(sqrt(sri)~diff_sum_heart_length*udoi +(1|dyadID), data=HRpos)
+mod1=lmer(sri~diff_total_length *udoi +(1|dyadID), data=HRpos)
+mod1.1=lmer(log(sri+1)~diff_total_length*udoi +(1|dyadID), data=HRpos)
+mod1.2=lmer(sqrt(sri)~udoi*Difference +(1|dyadID), data=HRpos)
 
 par(mfrow=c(1,3))
 qqnorm(residuals(mod1),ylab="Residuals")   #lame
@@ -44,6 +47,7 @@ qqline(residuals(mod1.2))
 
 plot(mod1.2)    #nice homoscedasticity
 
+shapiro.test((residuals(mod1.2)))
 # p-value
 Vcov <- vcov(mod1.2, useScale = FALSE)
 betas <- fixef(mod1.2)
@@ -52,34 +56,19 @@ zval <- betas / se
 pval <- 2 * pnorm(abs(zval), lower.tail = FALSE)
 round(cbind(betas, se, zval, pval), digits = 3)
 
-# udoi ***, body mass NS, interaction almost *
-# NB: interaction * with total length but not combined measurements or volume
+r2(mod1.2)
+#                 betas    se   zval  pval
+#(Intercept)      0.092 0.018  5.246 0.000
+#Difference      -0.002 0.002 -1.043 0.297
+#udoi             0.198 0.021  9.388 0.000
+#Difference:udoi  0.007 0.003  2.813 0.005
 
-# Remove interaction NS
 
-mod1.3=lmer(sqrt(sri)~diff_sum_heart_length+udoi+ (1|dyadID), data=HRpos)
 
-# p value
-summary(mod1.3)
-Vcov <- vcov(mod1.3, useScale = FALSE)
-betas <- fixef(mod1.3)
-se <- sqrt(diag(Vcov))
-zval <- betas / se
-pval <- 2 * pnorm(abs(zval), lower.tail = FALSE)
-round(cbind(betas, se, zval, pval), digits = 3)
-
-#                      betas    se   zval  pval
-#(Intercept)           0.059 0.013  4.438 0.000
-#diff_sum_heart_length 0.002 0.001  2.292 0.022  
-#udoi                  0.246 0.014 18.010 0.000 
-
-##### both udoi and delta *** with similar p values for diff volume and diff in combined 
-# measurements
 # ====> Caribou associate more often when HR overlap is bigger and big difference in size
 
 
 # Plots of the fixed effects ----------------------------------------------
-
 
 #Backtranformation of the sqrt of the response
 square <- function(x){
@@ -87,13 +76,16 @@ square <- function(x){
 }
 
 # random effect removed for plot, otherwise, no CI on the graph
-mod1.A=lm(sqrt(sri)~diff_sum_heart_length+udoi, data=HRpos)
+HRpos$UDOI=HRpos$udoi
+mod1.A=lm(sqrt(sri)~Difference*UDOI, data=HRpos)
 
 # Can't put them on the same line with ggplot2 so two steps 
-visreg(mod1.A,"udoi",trans= square, xlab="Home range overlap",ylab='SRI', 
-       partial=TRUE,fill.par=list(col="light blue"), gg=TRUE)
-visreg(mod1.A,"diff_sum_heart_length",xlab="Difference in body mass (estimated) - cm",
-       trans= square, ylab='SRI', partial=TRUE,fill=list(col="light blue"), gg=TRUE)
+v=visreg(mod1.A,"Difference", by='UDOI',trans= square, xlab="Difference in body size (cm)",
+ylab='Simple ratio index', partial=FALSE, gg=TRUE, overlay=TRUE, legend=FALSE,frame.plot=FALSE,xpd = TRUE,points=list(size=0.8, pch=20))+theme_classic() 
+
+
+black<- element_text( color = "black", size = 10)
+v + theme(axis.text = black)
 
 
 
