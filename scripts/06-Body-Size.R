13ce6ab4d9f8d921128d022d479c79aa2e24576b
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-22 13:17:48 -0400

    drop alt uses of diff_dyad

--- scripts/06-Body-Size.R
+++ scripts/06-Body-Size.R
@@ -29,15 +29,6 @@ lsdiff <- lapply(varls, diff_dyad, DT = bodyavg, id = idcol)
 diffs <- Reduce(function(x, y) merge(x, y, by = 'dyadID'),
                 lsdiff)
 
-## Other ways of using diff_dyad:
-# eg. for one variable
-# diff_dyad(DT = bodyavg, col = 'total_length', id = idcol)
-
-# one liner (nice)
-# Reduce(function(x, y) merge(x, y, on = 'dyadID'), lapply(varls, diff_dyad, DT = bodyavg, id = idcol))
-
-
-
 # Output ------------------------------------------------------------------
 saveRDS(diffs, 'output/06-body-size-diffs.Rds')
 
