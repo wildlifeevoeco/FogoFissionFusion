5563355fe25966f78abb5f1b0511517179460e54
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-22 13:08:18 -0400

    variables from prep locs

--- scripts/01-Prep-Locs.R
+++ scripts/01-Prep-Locs.R
@@ -6,27 +6,11 @@ lapply(libs, require, character.only = TRUE)
 
 
 # Input data --------------------------------------------------------------
-fogo <- fread('input/FogoCaribou.csv')
+fogo <- fread(fogo_path)
 
 
-# Set variables -----------------------------------------------------------
-# Bounds
-lowEastFogo <- 690000; highEastFogo <- 800000
-lowNorthFogo <- 5450000; highNorthFogo <- 6000000
 
-# Time zone 
-tz <- 'America/St_Johns'
-
-# Max moverate
-maxMoveRate <- 30000
-
-# Projected columns
-projCols <- c('EASTING', 'NORTHING')
-
-# Projection
-# TODO: fix this proj4string
-utm21N <- '+proj=utm +zone=21 ellps=WGS84'
-
+# Prepare -----------------------------------------------------------------
 # Prep Fogo data to merge 
 fogo[, idate := as.IDate(idate, tz = tz)]
 fogo[, itime := as.ITime(itime, tz = tz)]
@@ -53,20 +37,6 @@ fogo <- fogo[(lowEastFogo < EASTING & EASTING < highEastFogo) &
                (lowNorthFogo < NORTHING & EASTING < highNorthFogo)]
 
 # Subset ------------------------------------------------------------------
-# Remove  animals with malfunctioning collars
-dropIDYr <- c(
-  'FO2016006_2017', # not enough fixes 
-  'FO2016006_2018', # not enough fixes
-  'FO2017006_2019', # not enough fixes
-  'FO2017013_2018', # Dead animal
-  'FO2017004_2019', # not enough fixes
-  'FO2017007_2019'  # not enough fixes
-)
-
-dropID <- c(
-  'FO2016001' # Dead animal
-)
-
 fogo <- fogo[!(IDYr %in% dropIDYr) &
                !(ANIMAL_ID %in% dropID)]
 
--- variables.R
+++ variables.R
@@ -8,3 +8,36 @@ legend_path <- '../nl-landcover/input/FINAL_PRODUCT/FINAL_RC_legend.csv'
 body_path <- 'input/body.csv'
 
 
+
+# Bounds
+lowEastFogo <- 690000; highEastFogo <- 800000
+lowNorthFogo <- 5450000; highNorthFogo <- 6000000
+
+# Time zone 
+tz <- 'America/St_Johns'
+
+# Max moverate
+maxMoveRate <- 30000
+
+# Projected columns
+projCols <- c('EASTING', 'NORTHING')
+
+# Projection
+# TODO: fix this proj4string
+utm21N <- '+proj=utm +zone=21 ellps=WGS84'
+
+
+
+# Animals with malfunctioning collars or dead animals
+dropIDYr <- c(
+  'FO2016006_2017', # not enough fixes 
+  'FO2016006_2018', # not enough fixes
+  'FO2017006_2019', # not enough fixes
+  'FO2017013_2018', # Dead animal
+  'FO2017004_2019', # not enough fixes
+  'FO2017007_2019'  # not enough fixes
+)
+
+dropID <- c(
+  'FO2016001' # Dead animal
+)
