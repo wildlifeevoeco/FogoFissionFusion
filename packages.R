2b921b873ab21098f49d921aac56d15a73e0efa4
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-22 12:06:56 -0400

    fst packages.R

---
+++ packages.R
@@ -0,0 +1,12 @@
+# === Packages ------------------------------------------------------------
+library(data.table)
+library(ggplot2)
+library(rgdal)
+library(lubridate)
+library(raster)
+library(sp)
+library(vegan)
+library(landscapemetrics)
+library(spatsoc)
+library(adehabitatHR)
+
