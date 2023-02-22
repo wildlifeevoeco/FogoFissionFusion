c7243df8f0734d84df02fb369b3a816357eab1de
Alec L. Robitaille <robit.alec@gmail.com>
2023-02-22 13:08:08 -0400

    fst variables

---
+++ variables.R
@@ -0,0 +1,10 @@
+# === Variables -----------------------------------------------------------
+
+
+# Paths
+fogo_path <- 'input/FogoCaribou.csv'
+lc_path <- '../nl-landcover/output/fogo_lc.tif'
+legend_path <- '../nl-landcover/input/FINAL_PRODUCT/FINAL_RC_legend.csv'
+body_path <- 'input/body.csv'
+
+
